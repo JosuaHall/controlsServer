68f5d654c043a9e4599adc5d7a251a154b117f79
JosuaHall <halljosua@web.de>
2024-05-29 15:46:39 -0500

    logs added

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define CORS options to allow all origins
+// Define CORS options
 const corsOptions = {
-  origin: "https://hallshauscontrols.netlify.app/",
-  credentials: true, //access-control-allow-cryedentials:true
+  origin: "https://hallshauscontrols.netlify.app",
+  credentials: true,
   optionSuccessStatus: 200,
 };
 
@@ -18,22 +18,37 @@ app.use(cors(corsOptions));
 // Proxy route
 app.get("/api/endpoint", async (req, res) => {
   try {
-    console.log(process.env.ser_nr);
-    const response = await axios.get(`http://${process.env.ser_nr}`, {
+    const targetUrl = `http://${process.env.ser_nr}`;
+    console.log(`Target URL: ${targetUrl}`);
+
+    const response = await axios.get(targetUrl, {
       auth: {
         username: "pvserver",
         password: "pvwr",
       },
       headers: {
-        Authorization: req.header("Authorization"), // Pass authorization header if present
-        Accept: req.header("Accept"), // Pass accept header if present
+        Authorization: req.header("Authorization"),
+        Accept: req.header("Accept"),
         // Add any other headers you want to pass to the API server
       },
     });
     res.json(response.data);
   } catch (error) {
     console.error("Error proxying request:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    if (error.response) {
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
+      res.status(error.response.status).json({ error: error.response.data });
+    } else if (error.request) {
+      console.error("No response received:", error.request);
+      res
+        .status(500)
+        .json({ error: "No response received from target server" });
+    } else {
+      console.error("Error setting up request:", error.message);
+      res.status(500).json({ error: "Error setting up request" });
+    }
   }
 });
 
